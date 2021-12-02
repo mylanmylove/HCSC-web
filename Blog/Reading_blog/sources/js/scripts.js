8e1636d9dec2b8350938896374904973b893d78e
NamDT-146 <84223717+NamDT-146@users.noreply.github.com>
2021-12-02 22:00:24 +0700

    Update scripts.js

--- Blog/Reading_blog/sources/js/scripts.js
+++ Blog/Reading_blog/sources/js/scripts.js
@@ -1,116 +1,57 @@
-$.get('sources/blog/blog1/title.txt', function(data) {
-    $('#tt1').text(data)
-}, 'text');
-
-$.get('sources/blog/blog2/title.txt', function(data) {
-    $('#tt2').text(data)
-}, 'text');
-
-$.get('sources/blog/blog3/title.txt', function(data) {
-    $('#tt3').text(data)
-}, 'text');
-
 $.get('sources/blog/blog4/title.txt', function(data) {
-    $('#tt4').text(data)
-}, 'text');
-
-$.get('sources/blog/blog1/content.txt', function(data) {
-    var new_data = '';
-    var i = 0;
-    while (i < 475 || data[i] != ' ') {
-        new_data += data[i];
-        i++;
-    }
-
-    new_data += ' ...';
-
-    $('#pc1').text(new_data)
-}, 'text');
-
-$.get('sources/blog/blog2/content.txt', function(data) {
-    var new_data = '';
-    var i = 0;
-    while (i < 475 || data[i] != ' ') {
-        new_data += data[i];
-        i++;
-    }
-
-    new_data += ' ...';
-
-    $('#pc2').text(new_data)
-}, 'text');
-
-$.get('sources/blog/blog3/content.txt', function(data) {
-    var new_data = '';
-    var i = 0;
-    while (i < 475 || data[i] != ' ') {
-        new_data += data[i];
-        i++;
-    }
-
-    new_data += ' ...';
-
-    $('#pc3').text(new_data)
+  $('.title').text(data)
 }, 'text');
 
 $.get('sources/blog/blog4/content.txt', function(data) {
-    var new_data = '';
-    var i = 0;
-    while (i < 475 || data[i] != ' ') {
-        new_data += data[i];
-        i++;
-    }
+  var new_data = '';
+  var i = 0;
+  while(i < 200 || data[i] != '.')
+  {
+    new_data += data[i];
+    i++;
+  }
+ 
+  new_data += '.';
 
-    new_data += ' ...';
-
-    $('#pc4').text(new_data)
+  $('.summary').text(new_data)
 }, 'text');
 
+$.get('sources/blog/blog4/content.txt', function(data) {
+  $('.content').text(data)
+}, 'text');
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() { myFunction() };
+window.onscroll = function() {myFunction()};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
 
 // Get the offset position of the navbar
-var sticky = 75;
+var sticky = 30;
 
 function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        ext = true;
-        $('.nav').removeClass('sticky');
-    }
+  if (window.pageYOffset >= sticky)
+  {
+    navbar.classList.add("sticky")
+  }
+  else
+  {
+    $('.nav').removeClass('sticky');
+  }
 }
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 
 
 
 
 $(document).ready(function() {
     $('#menu').click(function() {
-        if (document.getElementById('menu-list').style.visibility === "visible") {
-            document.getElementById('menu-list').style.visibility = "hidden";
-        } else {
-            document.getElementById('menu-list').style.visibility = "visible";
-        }
+      if (document.getElementById('menu-list').style.visibility === "visible")
+      {
+        document.getElementById('menu-list').style.visibility = "hidden";
+      }
+      else{
+        document.getElementById('menu-list').style.visibility = "visible";
+      }
     });
 });
 
-
-
-// $(document).ready(
-//   function(){
-//     $('.col').waypoint(
-//       function(direction){
-//         if(direction == 'down'){
-//           $('nav').addClass('sticky');
-//         }
-//       },{
-//         offset: '100px'
-//       }
-//     )
-//   }
-// )
